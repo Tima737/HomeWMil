cce87b223a9855b39a748f3d390d2536d69cc2be
Boeing test <t.smoking@mail.ru>
2023-02-22 20:28:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+        public static void main(String[] args) {
+
+            int amount = 2000;
+            int rublMile = 20;
+
+
+            int bonus = amount / rublMile;
+
+            System.out.println(amount / rublMile);
+        }
+
+    }
+
+
+
